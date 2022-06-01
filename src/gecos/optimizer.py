Correct distance formula name

src/gecos/optimizer.py
@@ -367,7 +367,7 @@ class DefaultScoreFunction(ScoreFunction):
             self._tri_indices, matrix
         )
         self._contrast = contrast
-        if distance_formula not in ["CIEDE76", "CIEDE94", "CIEDE2000"]:
+        if distance_formula not in ["CIE76", "CIEDE94", "CIEDE2000"]:
             raise ValueError(
                 f"Unknown color distance formula '{distance_formula}'"
             )
@@ -392,7 +392,7 @@ class DefaultScoreFunction(ScoreFunction):
     @staticmethod
     def _calculate_distances(tri_indices, coord, distance_formula):
         ind1, ind2 = tri_indices
-        if distance_formula == "CIEDE76":
+        if distance_formula == "CIE76":
             return np.sqrt(
                 np.sum((coord[ind1, :] - coord[ind2, :])**2, axis=-1)
             )

tests/test_optimizer.py
@@ -87,7 +87,7 @@ def test_optimized_distances():
     matrix = SubstitutionMatrix(alph, alph, np.array(score_matrix))
     # Contrast factor is 0 to optimize only for pairwise distances
     score_func = gecos.DefaultScoreFunction(
-        matrix, contrast=0, distance_formula="CIEDE76"
+        matrix, contrast=0, distance_formula="CIE76"
     )
     ideal_distances = score_func._ideal_dist
     a_to_b_ref, a_to_c_ref, b_to_c_ref = ideal_distances